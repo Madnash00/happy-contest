bdec2f888bcf78b12a8af43b2081539d2be62d04
builtbyrikko <134959066+Madnash00@users.noreply.github.com>
2026-09-05 09:42:06 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.HAPPY_CONFIG = {
+  // Incolla qui l'URL /exec della distribuzione Apps Script API.
+  apiUrl: 'https://script.google.com/macros/s/AKfycbyqz0ZUzJuJCXpcBkkqt4wtWNUeGye-pzKVkd9TYKktczKltI8XBn8d0HSTGjjXio04bg/exec',
+  syncIntervalMs: 10000
+};
